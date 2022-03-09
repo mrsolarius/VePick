d50d2831166cd5649bd4af6dfa0078f9e8084cb2
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-09 21:44:49 +0100

    Feat : Mise en place partielle de l'abonnée

--- src/main/java/fr/litopia/model/Abonne.java
+++ src/main/java/fr/litopia/model/Abonne.java
@@ -1,8 +1,95 @@
 package fr.litopia.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Date;
 
 @Entity
-public class Abonne extends Client {
+public class Abonne {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "nom", length = 50)
+    private String nom;
+
+    @Column(name = "prenom", length = 50)
+    private String prenom;
+
+    @Column(name = "adresse")
+    private String adresse;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "date_fin")
+    private Date dateFin;
+
+    @Column(name = "credit_temps")
+    private Integer creditTemps;
+
+    @Column(name = "cb", length = 16)
+    private String cb;
+
+    public String getCb() {
+        return cb;
+    }
+
+    public void setCb(String cb) {
+        this.cb = cb;
+    }
+
+    public Integer getCreditTemps() {
+        return creditTemps;
+    }
+
+    public void setCreditTemps(Integer creditTemps) {
+        this.creditTemps = creditTemps;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
