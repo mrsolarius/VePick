58bd1f33dcbdcaf7108b910121297c71e9c011b5
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-09 21:36:23 +0100

    Feat : Mapping JPA de vélo

--- src/main/java/fr/litopia/model/Velo.java
+++ src/main/java/fr/litopia/model/Velo.java
@@ -1,12 +1,52 @@
 package fr.litopia.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import fr.litopia.model.enums.Etat;
+
+import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 @Table(name = "LesVelos")
 public class Velo {
     @Id
-    private int numero;
+    @Column(name = "numero", nullable = false)
+    private Long numero;
+
+    @Column(name = "mise_en_service", nullable = false, updatable = false)
+    private Date miseEnService;
+
+    @Enumerated
+    @Column(name = "etat", nullable = false)
+    private Etat etat;
+
+    @Column(name = "modele", nullable = false)
+    private String modele;
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public Etat getEtat() {
+        return etat;
+    }
+
+    public void setEtat(Etat etat) {
+        this.etat = etat;
+    }
+
+    public Date getMiseEnService() {
+        return miseEnService;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
 }
