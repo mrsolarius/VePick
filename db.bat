e7661d5dbbccefdb15e4c589e273ffdc41258c1e
julie <92156764+l3miage-turcjul@users.noreply.github.com>
2022-03-14 13:43:07 +0100

    Feat : Mise en place de la DB pour windows

--- db.bat
+++ db.bat
@@ -1,9 +1,2 @@
-docker run \
-    --name mariadb \
-    --rm --detach \
-    --env MARIADB_DATABASE=vepick \
-    --env MARIADB_USER=admin \
-    --env MARIADB_PASSWORD=admin \
-    --env MARIADB_ROOT_PASSWORD=admin  \
-    -p 6306:3306 \
-    mariadb
+docker run --name mariadb --rm --detach --env MARIADB_DATABASE=vepick --env MARIADB_USER=admin --env MARIADB_PASSWORD=admin --env MARIADB_ROOT_PASSWORD=admin -p 6306:3306 mariadb
+pause
