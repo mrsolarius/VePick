5e95afe78ecffde37714d5241b0a26fb6a5541fb
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-27 17:31:34 +0200

    Doc : Comentaire des tinyview

--- src/main/java/fr/litopia/views/struct/api/TinyView.java
+++ src/main/java/fr/litopia/views/struct/api/TinyView.java
@@ -1,5 +1,9 @@
 package fr.litopia.views.struct.api;
 
 public interface TinyView<T> {
+    /**
+     * Permet de démarer et récupérer le contenue de la tinyView
+     * @return T la valeur attendue ou null si la valeurs na pas étais trouvé
+     */
     T startAndGetValue();
 }
