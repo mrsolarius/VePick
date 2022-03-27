5c3a64bb0bb06f31dd7ed97fe88631590fe21ae0
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-27 16:51:42 +0200

    Doc : Comentaire du code de location

--- src/main/java/fr/litopia/model/Location.java
+++ src/main/java/fr/litopia/model/Location.java
@@ -27,10 +27,19 @@ public class Location {
     @JoinColumn(name = "velo_numero")
     private Velo velo;
 
+    /**
+     * Récupère le vélo associé à la location
+     * @return le vélo associé à la location
+     */
     public Velo getVelo() {
         return velo;
     }
 
+    /**
+     * Change le vélo associé à la location
+     * Vas ajouter un vélo à la location et vas ajouter une location à ce vélo si elle n'est pas déjà associée à un vélo
+     * @param velo le nouveau vélo
+     */
     public void setVelo(Velo velo) {
         if(this.velo==null && velo==null) return;
 
@@ -41,33 +50,63 @@ public class Location {
         this.velo = velo;
     }
 
+    /**
+     * Récupère la date de départ de la location
+     * @return la date de départ de la location
+     */
     public LocalDateTime getDepart() {
         return depart;
     }
 
+    /**
+     * Récupère le prix de la location
+     * @throws NullPointerException si la location n'a pas été clôturée et que le prix n'a pas été calculé
+     * @return le prix de la location
+     */
     public Double getPrix() {
         if (this.prix==null) throw new NullPointerException("Le prix de la location n'a pas été calculé");
         return prix;
     }
 
+    /**
+     * Récupère le temps de la location
+     * @return le temps de la location 0 si la location n'a pas été clôturée
+     */
     public Integer getTemps() {
         return temps;
     }
 
+    /**
+     * retourne le numéro de la location
+     * @return le numéro de la location
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Clôture la location
+     * @param bornette ou le vélo sera rendu
+     */
     public void cloreLocation(Bornette bornette) {
         velo.setBornette(bornette);
         temps=toIntExact(ChronoUnit.MINUTES.between(depart,LocalDateTime.now()));
         prix=temps*(velo.getModele().getPrixHoraire()/60);
     }
 
+    /**
+     * Permet de savoir si la location à était effectuer il y a moins de 5 minutes
+     * @return true si la location à était effectuer il y a moins de 5 minutes false sinon
+     */
     public Boolean isUnderFiveMinutes(){
         return toIntExact(ChronoUnit.MINUTES.between(depart,LocalDateTime.now()))<5;
     }
 
+    /**
+     * Permet de cloturer la location en cas de panne pour 0€ si le vélo et rendu hs moins de 5 minutes après la location
+     * @throws IllegalStateException si la location n'a pas été clôturée
+     * @param bornette ou le vélo sera rendu
+     */
     public void clotureLocationHSUnderFiveMinutes(Bornette bornette){
         if(isUnderFiveMinutes()) {
             velo.setBornette(bornette);
