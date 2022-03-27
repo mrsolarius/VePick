9a3be00e3fe804d7586b4ba23fa62645da7d538c
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-27 17:07:11 +0200

    Doc : Comentaire du code de Station

--- src/main/java/fr/litopia/model/Station.java
+++ src/main/java/fr/litopia/model/Station.java
@@ -20,43 +20,84 @@ public class Station {
     @OneToMany(mappedBy = "pk.station", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Bornette> bornettes = new LinkedHashSet<>();
 
+    /**
+     * Constructeur de vide de la classe Station
+     */
     public Station() {}
 
+    /**
+     * Constructeur de la classe Station
+     * @param adresse adresse de la station
+     */
     public Station(String adresse){
-
         this.vStatus = VStatus.VNUL;
     }
 
+    /**
+     * Constructeur de la classe Station
+     * @param adresse adresse de la station
+     * @param vStatus status de la station
+     */
     public Station (String adresse, VStatus vStatus){
         this.setAdresse(adresse);
         this.vStatus = vStatus;
     }
 
+    /**
+     * Setter de l'adresse de la station
+     * @throws IllegalArgumentException si l'adresse est trop longue (plus de 255 caractères)
+     * @param adresse adresse de la station
+     */
     private void setAdresse(String adresse){
         if(adresse.length()>255) throw new IllegalArgumentException("Adresse trop longue");
         this.adresse = adresse;
     }
 
+    /**
+     * Getter de la liste des bornettes de la station
+     * @return liste des bornettes de la station
+     */
     public Set<Bornette> getBornettes() {
         return bornettes;
     }
 
+    /**
+     * Setter de la liste des bornettes de la station
+     * @param bornettes liste des bornettes de la station
+     */
     public void setBornettes(Set<Bornette> bornettes) {
         this.bornettes = bornettes;
     }
 
+    /**
+     * Getter du status de la station
+     * @return status de la station
+     */
     public VStatus getVStatus() {
         return vStatus;
     }
 
+    /**
+     * Setter du status de la station
+     * @param vStatus status de la station
+     */
     public void setVStatus(VStatus vStatus) {
         this.vStatus = vStatus;
     }
 
+    /**
+     * Getter de l'adresse de la station
+     * @return adresse de la station
+     */
     public String getAdresse() {
         return adresse;
     }
 
+    /**
+     * Redéfinition de la méthode equals
+     * @param obj l'objet à comparer
+     * @return true si les objets sont égaux, false sinon
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
