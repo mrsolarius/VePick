909413e97baf87138d9c32d06f9116739b1416d2
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-27 17:14:45 +0200

    Doc : Comentaire du répo de bornette

--- src/main/java/fr/litopia/respository/api/BornetteRepository.java
+++ src/main/java/fr/litopia/respository/api/BornetteRepository.java
@@ -8,21 +8,33 @@ import fr.litopia.model.enums.Etat;
 import java.util.Set;
 
 public interface BornetteRepository extends Repository<Bornette, BornettePK> {
-    //@Todo ajouter des methods utiles pour la gestion des bornettes
+    /**
+     * Renvois le dernier numéro de bornette utilisé pour une station donnée
+     * @param s la station
+     * @return le dernier numéro de bornette utilisé pour une station donnée
+     */
     long getLastIdInStation(Station s);
 
     /**
      * renvoie les bornettes d'une station
+     * @param s la station
+     * @return les bornettes d'une station
      */
     Set<Bornette> getBornettesStationWithBike(Station s);
 
     /**
-     * renvoie les bornettes à l'état e qui contiennent des vélos à l'état e
+     * Renvoie les bornettes à l'état e qui contiennent des vélos à l'état e
+     * @param s la station
+     * @param e l'état
+     * @return les bornettes à l'état e qui contiennent des vélos à l'état e
      */
     Set<Bornette> getBornettesStationWithBike(Station s, Etat e);
 
     /**
      * renvoie les bornettes à l'état e qui contiennent des vélos à l'état e
+     * @param e l'état
+     * @param s la station
+     * @return les bornettes à l'état e qui contiennent des vélos à l'état e
      */
     Set<Bornette> getBornettesStationWithoutBike(Station s, Etat e);
 
