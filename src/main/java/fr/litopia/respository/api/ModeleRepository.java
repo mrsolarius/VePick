e0f29f53e3fde962f53e57218da682c5fc94aa12
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-27 17:17:28 +0200

    Doc : suppression des todo inutile

--- src/main/java/fr/litopia/respository/api/ModeleRepository.java
+++ src/main/java/fr/litopia/respository/api/ModeleRepository.java
@@ -3,5 +3,4 @@ package fr.litopia.respository.api;
 import fr.litopia.model.Modele;
 
 public interface ModeleRepository extends Repository<Modele, String> {
-    //@Todo ajouter des methods utiles pour la gestion des modèles
 }
--- src/main/java/fr/litopia/respository/api/VeloRepository.java
+++ src/main/java/fr/litopia/respository/api/VeloRepository.java
@@ -3,5 +3,4 @@ package fr.litopia.respository.api;
 import fr.litopia.model.Velo;
 
 public interface VeloRepository extends Repository<Velo,Long> {
-    //@Todo ajouter des methods utiles pour la gestion des velos
 }
